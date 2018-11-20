a973a83960a96cb6a9a92f4ddf1c373e534f2cf0
FX Wood <fxwood2@gmail.com>
2018-11-20 22:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function takeANumber(deliLine, name)  {
   return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`
 }
+
+function nowServing(deliLine) {
+  return deliline.length > 0 ? deliLine.unshift() || "The line is currently empty."
+}
